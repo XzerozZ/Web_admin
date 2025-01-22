aa00116bb1a691f37fd6fc532e055e204da5e347
XzerozZ <111548296+XzerozZ@users.noreply.github.com>
2025-01-22 23:52:58 +0700

    ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+      <body id="header-login"
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
